360640b65a3f66db80ae91bb696996daa88e1370
ykrods <890082+ykrods@users.noreply.github.com>
2024-05-16 21:54:39 +0900

    Update server

--- server.js
+++ server.js
@@ -1,65 +1,111 @@
 /**
  * Static HTTP server with routing
  */
-import fs from 'fs/promises';
-import http from 'http';
-import path from 'path';
-import process from 'process';
+import fs from 'node:fs/promises';
+import http from 'node:http';
+import path from 'node:path';
+import process from 'node:process';
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
 
-const STATIC_DIR = path.join('example', 'dist');
-
-const STATIC_FILES = [
-  "Article.js",
-  "Article.js.map",
-  "Login.js",
-  "Login.js.map",
-  "Query.js",
-  "Query.js.map",
-  "index.html",
-  "main.js",
-  "main.js.map",
-  "main2.js",
-  "main2.js.map",
-];
-
-function getFileName(url) {
-  const s = STATIC_FILES.find(f => '/' + f === url);
-  if (s) {
-    return s;
-  }
-  // rewrite to index.html
-  return 'index.html';
+function log(code, request) {
+  console.log(`[${(new Date()).toISOString()}] (${code}) ${request.method} ${request.url}`);
 }
 
-function getContentType(filename) {
-  const extMap = {
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.map': 'application/octet-stream',
-    '.html': 'text/html',
-  }
-  return extMap[path.extname(filename)] || 'text/plain';
+function getPathname(reqUrl) {
+  // XXX: host and port are not referended. so use fixed values.
+  const url = new URL(`http://localhost:8888${reqUrl}`);
+  return decodeURI(url.pathname);
 }
 
-export const server = http.createServer(async (request, response) => {
-  console.log(`[${(new Date()).toISOString()}] ${request.method} ${request.url}`);
+/**
+ *  @param {Object} options
+ *  @param {string} options.dir - directory to serve
+ *  @param {Object.<string, string>} options.extMap - mapping file extensions to MIME types
+ *  @param {(path: string) => string} [options.rewrite] - (optional) rewrite path function
+ */
+function createServer(options) {
+  const getContentType = (contentPath) => {
+    return options.extMap[path.extname(contentPath)] || null;
+  };
+  const root = path.resolve(options.dir);
 
-  const fileName = getFileName(request.url);
-  const contentType = getContentType(fileName);
+  const handler = async (request, response) => {
+    if (request.method.toUpperCase() !== "GET") {
+      response.writeHead(405, {'Content-Type': 'text/plain'});
+      response.end("Method Not Allowed", 'utf-8');
+      log(405, request);
+      return;
+    }
 
-  // console.log(`${fileName}, ${contentType}`);
+    let p = getPathname(request.url);
+    if (options.rewrite instanceof Function) {
+      p = options.rewrite(p);
+    }
 
-  const content = await fs.readFile(path.join(STATIC_DIR, fileName), 'utf-8');
+    const contentPath = path.join(root, p);
+    if (!contentPath.startsWith(root)) {
+      throw new Error("invalid request");
+    }
 
-  response.writeHead(200, {'Content-Type': contentType});
-  response.end(content, 'utf-8');
+    try {
+      const content = await fs.readFile(contentPath);
+      const contentType = getContentType(contentPath);
+      if (contentType === null) {
+        throw new Error("Unknown file type");
+      }
+
+      response.writeHead(200, {'Content-Type': contentType });
+      response.end(content);
+      log(200, request);
+    } catch (err) {
+      if (err.code == "ENOENT") {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end("Not Found", 'utf-8');
+        log(404, request);
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  return http.createServer(async (request, response) => {
+    try {
+      await handler(request, response);
+    } catch(error) {
+      response.writeHead(500, {'Content-Type': 'text/plain'});
+      response.end("Internal Server Error", 'utf-8');
+      console.error(error);
+      log(500, request);
+    }
+  });
+}
+
+export const server = createServer({
+  dir: "example/dist",
+  extMap: {
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".json": "text/json",
+    ".map": "application/octet-stream",
+    ".html": "text/html",
+  },
+  rewrite(pathname) {
+    // rewrite to index.html
+    const rewriteConds = [
+      path.extname(pathname) === "",
+    ];
+    if (rewriteConds.some(v => v)) {
+      return "/index.html";
+    }
+    return pathname;
+  }
 });
 
 // Run server
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  const host = "localhost";
   const port = 8080;
-  server.listen(port);
-  console.log(`Server running on ${port}`);
+  server.listen(port, host);
+  console.log(`Server running on ${host}:${port}`);
 }
